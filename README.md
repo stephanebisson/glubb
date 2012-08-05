6081a249dd01d09779105a123817cd84cc17f8d2
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-08-05 13:17:09 +0800

    first functional-ish version

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ glubb
 Some kind of twitterishy-thingy
 
 checking out meteor
+
+--
+
+## issues
+
+- cannot find a clean way to create a geospacial index
+- cannot find the way to use the geoNear command
--- glubb/glubb.js
+++ glubb/glubb.js
@@ -1,38 +1,25 @@
 if (Meteor.is_client) {
     var messages = new Meteor.Collection('messages');
 
+    Session.set('loc', [0,0]);
     window.navigator.geolocation.watchPosition(function(pos) {
         Session.set('loc', [pos.coords.longitude, pos.coords.latitude]);
     });
-
-    messages.remove({});
-    messages.insert({
-        text: 'hello from beijing',
-        loc: [116.430, 39.93],
-        timestamp: new Date(2012, 7, 2, 17, 50, 1, 2).getTime()
-    });
-    messages.insert({
-        text: 'hello from wuhan',
-        loc: [114.31, 30.71],
-        timestamp: new Date(2012, 7, 4, 17, 52, 1, 2).getTime()
-    });
-    messages.insert({
-        text: 'hello from xian',
-        loc: [108.955, 34.27],
-        timestamp: new Date(2012, 7, 4, 17, 55, 1, 2).getTime()
+    
+    Meteor.autosubscribe(function(){
+        Meteor.subscribe('messages', Session.get('loc'));
     });
 
-
-
-    var defaultLoc = [0, 0];
     Template.add.events = {
         'click #add': function() {
+            var textarea = $('#theText');
+            if (!textarea.val()) { return; }
             messages.insert({
-                text: $('#theText').val(),
-                timestamp: Date.now(),
-                loc: Session.get('loc') || defaultLoc
+                text: textarea.val(),
+                loc: Session.get('loc'),
+                timestamp: Date.now()
             });
-            $('#theText').val('');
+            textarea.val('');
         }
     };
 
@@ -44,15 +31,15 @@ if (Meteor.is_client) {
             now.setMilliseconds(0);
             return now.getTime();
         };
+        
+    var distance = function(loc1, loc2){
+        var p1 = new LatLon(loc1[0], loc1[1]);
+        var p2 = new LatLon(loc2[0], loc2[1]);
+        return p1.distanceTo(p2);
+    };
 
     Template.view.messages = function() {
-        // var all = messages.find({timestamp: {$gt: today()}}, {sort: {timestamp: -1}}).fetch();
-        var all = messages.find({}, {
-            sort: {
-                timestamp: -1
-            }
-        }).fetch();
-        return all.slice(0, 7);
+        return messages.find({});
     };
 
     Template.view.format = function(d) {
@@ -62,9 +49,7 @@ if (Meteor.is_client) {
     Template.view.distance = function(loc) {
         var myloc = Session.get('loc');
         if (myloc) {
-            var p1 = new LatLon(loc[0], loc[1]);
-            var p2 = new LatLon(myloc[0], myloc[1]);
-            return p1.distanceTo(p2);
+            return distance(loc, myloc);
         }
         return '?';
     };
@@ -72,4 +57,49 @@ if (Meteor.is_client) {
 
 if (Meteor.is_server) {
     var messages = new Meteor.Collection('messages');
+    
+    var getMongoCollectionDb = function(collection, callback) {
+        var ticket = Meteor.setInterval(function(){
+            if (collection._driver.mongo.db) {
+                Meteor.clearInterval(ticket);
+                callback(collection._driver.mongo.db);
+            }
+        }, 200);
+    };
+    
+    var dummyData = function(){
+        messages.remove({});
+        messages.insert({
+            text: 'hello from beijing',
+            loc: [116.430, 39.93],
+            timestamp: new Date(2012, 7, 2, 17, 50, 1, 2).getTime()
+        });
+        messages.insert({
+            text: 'hello from wuhan',
+            loc: [114.31, 30.71],
+            timestamp: new Date(2012, 7, 4, 17, 52, 1, 2).getTime()
+        });
+        messages.insert({
+            text: 'hello from xian',
+            loc: [108.955, 34.27],
+            timestamp: new Date(2012, 7, 4, 17, 55, 1, 2).getTime()
+        });
+        
+    };
+    
+    Meteor.startup(function(){
+        //dummyData();
+
+        Meteor.publish('messages', function(where){
+            return messages.find({loc: {$near: where}}, {limit: 3});
+        });
+
+        getMongoCollectionDb(messages, function(db){
+            db.collection('messages', function(error, mongo){
+                mongo.db.ensureIndex('messages', {loc: '2d'}, {}, function(){
+                    // console.log('2d index ensured');
+                });
+            });
+        });
+    });
 }
