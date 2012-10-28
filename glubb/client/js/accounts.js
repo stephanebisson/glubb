99eb6cf89e009897a592169c8ba5498e82595aeb
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-10-28 17:07:08 +0800

    oauth configuration

---
+++ glubb/client/js/accounts.js
@@ -0,0 +1,30 @@
+Accounts.loginServiceConfiguration.insert({
+  service: "facebook",
+  clientId: "",
+  secret: ""
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: "github",
+  clientId: "a257760aa060f11129af",
+  secret: "42177eb3c042745b5d3b433f6b5e7ef8dd9a0514"
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: "google",
+  clientId: "296941079441.apps.googleusercontent.com",
+  secret: "fbvdWVk73XKVqjPXlxDNR5cq"
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: "twitter",
+  clientId: "MCLWtf6vfyCuNokxcE5uTA",
+  secret: "0K9uCwJunSdB2hZAkvAVre6RsnZlv65TNs6nrtsMk"
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: "weibo",
+  clientId: "4259203338",
+  secret: "c0debf0491b7ac2e2eef0354000171b7"
+});
+
